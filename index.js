9f5e10564f5f67bd651d5fa1ffa5bb2682f98aa0
smrubin <sam@storyblocks.com>
2018-10-14 23:50:16 -0400

    Creates a working LazyLoader class

--- index.js
+++ index.js
@@ -1,3 +1,64 @@
-module.exports = (moduleName) => {
-	return `Hello ${moduleName}`;
+export default class LazyLoader {
+
+	constructor(selector, rootMargin = '200px' ) {
+		this.selector = selector;
+		this.rootMargin = rootMargin;
+		this.observeTargets();
+	}
+
+	getTargetElements() {
+		return document.querySelectorAll(this.selector);
+	}
+
+	getIntersectionObserverOptions() {
+		return {
+			root: null,
+			rootMargin: this.rootMargin,
+			threshold: 0,
+		}
+	}
+
+	observeTargets() {
+		const targetElements = this.getTargetElements();
+
+		if (!targetElements || !targetElements.length) {
+			// Error or no matching elements found
+			return;
+		}
+
+		// Create new IO with lazy load handler
+		const observer = new IntersectionObserver(this.handleIntersection, this.getIntersectionObserverOptions());
+
+		// observe each one
+		Array.from(targetElements)
+			.filter(this.isElementLazyLoadable)
+			.forEach(el => {
+				observer.observe(el)
+			});
+	}
+
+	isElementLazyLoadable(el) {
+		return el.nodeName.toLowerCase() === 'img' && el.attributes['data-lazyload'];
+	}
+
+	// this will be the handler that actually does the lazy loading
+	handleIntersection(entries, observer) {
+
+		entries.forEach(entry => {
+
+			/**
+			 * When the IntersectionObserver is instantiated the callback is ran once
+			 * as a detection for whether the element is in view or not.
+			 */
+			if (!entry.isIntersecting) { // todo - check on this
+				return;
+			}
+
+			//load the actual image by making the src the data attribute
+			entry.target.src = entry.target.attributes['data-lazyload'].value;
+			observer.unobserve(entry.target);
+
+		});
+	}
+
 };
